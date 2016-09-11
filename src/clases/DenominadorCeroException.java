f3dd4904c1e897a21cb077bbda83ded7eb13e888
brayan <brayan3288@gmail.com>
2016-09-11 18:51:39 -0500

    interfaz terminada y funcionando

---
+++ src/clases/DenominadorCeroException.java
@@ -0,0 +1,15 @@
+
+package clases;
+
+/**
+ *
+ * @author baltamar3
+ */
+public class DenominadorCeroException extends Exception{
+
+    public DenominadorCeroException() {
+        super("No se permite denominadores en cero");
+        
+    }
+    
+}
---
+++ src/clases/Fraccionario.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author baltamar3
+ */
+public class Fraccionario {
+
+    private int numerador;
+    private int denominador;
+
+    public Fraccionario(int numerador, int denominador) throws DenominadorCeroException {
+        this.numerador = numerador;
+        this.denominador = denominador;
+
+        if (denominador == 0) {
+            throw new DenominadorCeroException();
+        }
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public Fraccionario sumar(Fraccionario f2) throws DenominadorCeroException {
+        Fraccionario f;
+        int num, den;
+
+        num = this.numerador * f2.denominador + this.denominador * f2.numerador;
+        den = this.denominador * f2.denominador;
+        f = new Fraccionario(num, den);
+        return f;
+    }
+
+    public Fraccionario resta(Fraccionario f2) throws DenominadorCeroException {
+        Fraccionario f;
+        int num, den;
+
+        num = this.numerador * f2.denominador - this.denominador * f2.numerador;
+        den = this.denominador * f2.denominador;
+        f = new Fraccionario(num, den);
+        return f;
+    }
+
+    public Fraccionario multiplicacion(Fraccionario f2) throws DenominadorCeroException {
+        Fraccionario f;
+        int num, den;
+
+        num = this.numerador * f2.numerador;
+        den = this.denominador * f2.denominador;
+        f = new Fraccionario(num, den);
+        return f;
+    }
+
+    public Fraccionario division(Fraccionario f2) throws DenominadorCeroException {
+        Fraccionario f;
+        int num, den;
+
+        num = this.numerador * f2.denominador;
+        den = this.denominador * f2.numerador;
+        f = new Fraccionario(num, den);
+        return f;
+    }
+
+}
